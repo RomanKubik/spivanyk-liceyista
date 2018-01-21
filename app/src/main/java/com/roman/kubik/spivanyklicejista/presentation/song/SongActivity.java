c6ec570bbe5621bca1cfa3b1516b1d5a1272dd1c
Roman Kubik <kubik@Romans-MacBook-Pro.local>
2018-01-21 09:05:31 +0200

    Add song activity. No design

--- app/src/main/java/com/roman/kubik/spivanyklicejista/presentation/song/SongActivity.java
+++ app/src/main/java/com/roman/kubik/spivanyklicejista/presentation/song/SongActivity.java
@@ -3,6 +3,7 @@ package com.roman.kubik.spivanyklicejista.presentation.song;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.roman.kubik.spivanyklicejista.Constants;
 import com.roman.kubik.spivanyklicejista.R;
@@ -13,6 +14,8 @@ import com.roman.kubik.spivanyklicejista.presentation.song.di.SongModule;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+
 /**
  * Created by kubik on 1/20/18.
  */
@@ -20,6 +23,11 @@ import javax.inject.Inject;
 public class SongActivity extends BaseActivity implements SongContract.View {
 
     private static final String TAG = SongActivity.class.getSimpleName();
+
+    @BindView(R.id.tvTitle)
+    TextView tvTitle;
+    @BindView(R.id.tvLyrics)
+    TextView tvLyrics;
 
     @Inject
     SongContract.Presenter presenter;
@@ -34,7 +42,9 @@ public class SongActivity extends BaseActivity implements SongContract.View {
 
     @Override
     public void showSong(Song song) {
-        Log.d(TAG, "showSong: " + song.getTitle());
+        Log.d(TAG, "showSong: " + song.getTitle() + " " + song.getLyrics());
+        tvTitle.setText(song.getTitle());
+        tvLyrics.setText(song.getLyrics());
     }
 
     @Override
