969d27a8fff1dfd1f8a61a370f8ac03a6a1b8c44
Roman Kubik <kubik@Romans-MacBook-Pro.local>
2018-01-21 09:13:12 +0200

    Add class for chords recognition

---
+++ domain/src/main/java/com/roman/kubik/spivanyklicejista/domain/utils/ChordsRecognizer.java
@@ -0,0 +1,16 @@
+package com.roman.kubik.spivanyklicejista.domain.utils;
+
+/**
+ * Created by kubik on 1/21/18.
+ */
+
+public class ChordsRecognizer {
+
+    public String markChordsInText(String text) {
+
+
+        return text;
+    }
+
+
+}
