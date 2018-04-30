1f5e7a62614acd30e94a1d7d8fb811effcc936df
Roman Kubik <roman89kubik@gmail.com>
2018-04-30 14:25:55 +0300

    Add ordering

--- data/src/main/java/com/roman/kubik/spivanyklicejista/data/song/SongDao.java
+++ data/src/main/java/com/roman/kubik/spivanyklicejista/data/song/SongDao.java
@@ -19,10 +19,10 @@ import io.reactivex.Single;
 @Dao
 public interface SongDao {
 
-    @Query("SELECT * FROM song")
+    @Query("SELECT * FROM song ORDER BY song.title")
     Single<List<SongEntity>> getAll();
 
-    @Query("SELECT * FROM song WHERE song.category_id = :categoryId")
+    @Query("SELECT * FROM song WHERE song.category_id = :categoryId ORDER BY song.title")
     Single<List<SongEntity>> getAllByCategory(int categoryId);
 
     @Query("SELECT * FROM song WHERE song.title LIKE :title")
